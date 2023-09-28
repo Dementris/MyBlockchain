4174492a58f0433b774e97e7a46d2eb1b74b2f9e
Dementris <bdlap.shl@gmail.com>
2023-09-28 20:10:11 +0300

    added blockchain tested PoW

--- blockchain.py
+++ blockchain.py
@@ -7,33 +7,40 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.transaction = []
-        self.new_block(100,1)
+        self.new_block(734,'Shlapak')
 
     def new_block(self, proof, previous_hash=None):
         block = {
-            'index': len(self.chain) + 1,
+            'index': len(self.chain)+1,
             'timestamp': time(),
             'transaction': self.transaction,
             'proof': proof,
-            'previous_hash': previous_hash
+            'previous_hash': previous_hash or self.hash(self.last_block)
         }
         self.transaction = []
+        self.chain.append(block)
         return block
 
-    def new_transaction(self):
-        pass
+    def new_transaction(self,sender, recipient, amount):
+        self.transaction.append({
+            'sender':sender,
+            'recipient':recipient,
+            'amount':amount
+        }
+        )
+        return self.last_block['index']+1
 
-    def proof_of_work(self,last_proof):
+    def proof_of_work(self,previous_hash):
         proof = 0
-        while self.valid_proof(last_proof,proof) is False:
+        while self.valid_proof(previous_hash,proof) is False:
             proof += 1
         return proof
 
     @staticmethod
-    def valid_proof(last_proof,proof):
-        guess = f'{last_proof}{proof}'.encode()
+    def valid_proof(previous_hash,proof):
+        guess = f'{previous_hash}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
+        return guess_hash[:4] == "0703"
 
     @staticmethod
     def hash(block):
--- main.py
+++ main.py
@@ -1,13 +1,61 @@
-from fastapi import FastAPI
+import hashlib
+import json
+from uuid import uuid4
+from fastapi import FastAPI, Request, Response
+from blockchain import Blockchain
 
 app = FastAPI()
+node_identifier = str(uuid4()).replace('-','')
+blockchain = Blockchain()
+@app.get("/mine")
+async def mine():
+    last_block = blockchain.last_block
+    previous_hash = last_block['previous_hash']
+    proof = blockchain.proof_of_work(previous_hash)
 
+    blockchain.new_transaction(
+        sender='0',
+        recipient=node_identifier,
+        amount=1
+    )
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof,previous_hash)
+    response = {
+        'message': "New block Forged",
+        'index': block['index'],
+        'transaction': block['transaction'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+        'current_hash': blockchain.hash(block)
+    }
+    return response
 
+@app.post("/transactions/new")
+async def new_transaction(info: Request):
+    values = await info.json()
+    required = ['sender','recipient','amount']
+    if not all(k in values for k in required):
+        return Response(content="missing values", status_code=400)
+    index = blockchain.new_transaction(values['sender'],values['recipient'],values['amount'])
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return response
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/chain")
+async def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return response
+@app.get("/hello")
+async def hello():
+    message = "hello world".encode()
+    response = {
+        "1": f'{message}',
+        "2": f'{hashlib.sha256(message).hexdigest()}',
+        "3": f'{hashlib.sha256(message).hexdigest()}',
+        "4": f'{hashlib.sha256(message).hexdigest()}',
+        "5": f'{hashlib.sha256(message).hexdigest()}',
+    }
+    return response
