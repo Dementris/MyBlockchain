a4ace3015c630a9e3c6e197569722d6c67bd0e13
Dementris <bdlap.shl@gmail.com>
2023-09-28 11:36:40 +0300

    blockchain skeleton

---
+++ blockchain.py
@@ -0,0 +1,45 @@
+from time import time
+import json
+import hashlib
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.transaction = []
+        self.new_block(100,1)
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transaction': self.transaction,
+            'proof': proof,
+            'previous_hash': previous_hash
+        }
+        self.transaction = []
+        return block
+
+    def new_transaction(self):
+        pass
+
+    def proof_of_work(self,last_proof):
+        proof = 0
+        while self.valid_proof(last_proof,proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof,proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
